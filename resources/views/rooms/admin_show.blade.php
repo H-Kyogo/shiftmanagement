bf071987d90af9a8f7ca0709e6f38319221e6a6d
H-Kyogo <K-hayashi0613@outlook.jp>
2023-10-13 04:57:09 +0900

    adminのcss追加・編集

--- resources/views/rooms/admin_show.blade.php
+++ resources/views/rooms/admin_show.blade.php
@@ -1,71 +1,84 @@
 <x-admin-layout>
-    <div class="Members">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-green-500 overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 text-gray-900">
+                
+                <div class="Members">
+                    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                            <div class="p-6 text-gray-900">
+                                
+                                <a>---</a>
+                                <a class="text-black">{{ $room->name }}</a>
+                                <a>---</a>
+                                
+                            </div>
+                        </div>
+                    </div>
                     
-                    <h1>{{ $room->name }}</h1>
+                    <br>
                     
-                </div>
-            </div>
-        </div>
-        
-        <br>
-        
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <div class="container">
-                        <p>{{ $room->description }}</p>
-                        <h2>---Members---</h2>
-                        <ul class="list-group">
-                            @foreach($room->users as $user)
-                            <li class="list-group-item">{{ $user->name }}</li>
-                            @endforeach
-                        </ul>
-                        <h2>---------------------</h2>
+                    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                            <div class="p-6 text-gray-900">
+                                <div class="container">
+                                    <p>{{ $room->description }}</p>
+                                    <h2 class="text-black">---Members---</h2>
+                                    <ul class="list-group">
+                                        @foreach($room->users as $user)
+                                        <li class="text-blue-600", class="list-group-item">{{ $user->name }}</li>
+                                        @endforeach
+                                    </ul>
+                                    <h2 class="text-black">---------------------</h2>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </div>
-    
-    <br>
-    
-    <div class="informations">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <a>---お知らせ一覧---</a>
-                    @foreach($room->posts as $post)
-                    <br>
-                    <a href="/admin/posts/{{ $post->id }}">{{ $post->title }}</a>
-                    @endforeach
-                    <h2>---------------------</h2>
+                
+                <br>
+                
+                <div class="informations">
+                    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                            <div class="p-6 text-gray-900">
+                                <a class="text-black">---お知らせ一覧---</a>
+                                @foreach($room->posts as $post)
+                                <br>
+                                <a class="text-blue-600", href="/admin/posts/{{ $post->id }}">{{ $post->title }}</a>
+                                @endforeach
+                                <h2 class="text-black">---------------------</h2>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </div>
-    
-    <br>
-    
-    <div class="newinformations">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <a href="{{ url('admin/rooms/' . $room->id . '/information/create')}}">---新たなお知らせを作成する---</a>
+                
+                <br>
+                
+                <div class="newinformations">
+                    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                            <div class="p-6 text-gray-900">
+                                <a class="text-black">---</a>
+                                <a class="text-blue-600", href="{{ url('admin/rooms/' . $room->id . '/information/create')}}">新たなお知らせを作成する</a>
+                                <a class="text-back">---</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </div>
-    
-    <br>
-    
-    <div class="back">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <a href="{{ url('admin/rooms/index')}}">---戻る---</a>
+                
+                <br>
+                
+                <div class="back">
+                    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                            <div class="p-6 text-gray-900">
+                                <a class="text-black">---</a>
+                                <a class="text-blue-600" href="{{ url('admin/rooms/index')}}">戻る</a>
+                                <a class="text-black">---</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/rooms/create.blade.php
+++ resources/views/rooms/create.blade.php
@@ -1,43 +1,57 @@
 <x-admin-layout>
     <div class="container">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="bg-green-500 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     
-                    <h1>---ルームの作成---</h1>
-                   <br>
-                   
-                   <form action="{{ route('admin.rooms.store') }}" method="post">
-                        @csrf
-                        <div class="mb-3">
-                            <label for="name" class="form-label">Room Name</label>
-                            <input type="text" class="form-control" id="name" name="name" required>
-                            
-                            @error('name')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
+                    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                            <div class="p-6 text-gray-900">
+                                
+                                <h1>---ルームの作成---</h1>
+                               <br>
+                               
+                               <form action="{{ route('admin.rooms.store') }}" method="post">
+                                    @csrf
+                                    <div class="mb-3">
+                                        <label for="name" class="form-label">Room Name</label>
+                                        <input type="text" class="form-control" id="name" name="name" required>
+                                        
+                                        @error('name')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                        @enderror
+                                    </div>
+                                    
+                                    <br>
+                                    
+                                    <a>---</a>
+                                    <button class="text-blue-600", type="submit" class="btn btn-primary">ルームを作成する</button>
+                                    <a>---</a>
+                                    
+                                </form>
+                               
+                            </div>
                         </div>
-                        <br>
-                        <button type="submit" class="btn btn-primary">---ルームを作成する---</button>
-                    </form>
-                   
+                    </div>
+                    
+                    <br>
+                    
+                    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                            <div class="p-6 text-gray-900">
+                                
+                                <a>---</a>
+                                <a class="text-blue-600", href="{{ url('admin/rooms/index')}}">戻る</a>
+                                <a>---</a>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
         </div>
-        
-        <br>
-        
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    
-                    <a href="{{ url('admin/rooms/index')}}">---戻る---</a>
-                    
-                </div>
-            </div>
-        </div>
-        
     </div>
 </x-admin-layout>
